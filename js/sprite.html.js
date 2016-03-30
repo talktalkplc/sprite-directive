9c449bce5f7c32c80b3a1f5c692317266154047d
Jim Drury <jim.drury@talktalkplc.com>
2016-03-30 17:02:13 +0100

    tidying up locally generated code

--- js/sprite.html.js
+++
@@ -1,4 +0,0 @@
-angular.module("APP/sprite/html/sprite.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("APP/sprite/html/sprite.html",
-    "<img ng-class=\"'{{img}}'\" alt=\"{{alt}}\" src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=\">");
-}]);
